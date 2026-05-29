82a223c9c46f10e5d5057d2791b7c566c6bfd140
aditya1219-beep <swamiaditya1219@gmail.com>
2026-05-29 16:47:33 +0530

    Add files via upload

---
+++ day2/svaq6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    int digit;
+    int rev=0;
+    printf("enter number:");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        digit=n%10;
+        rev=rev*10+digit;
+        n=n/10;
+    }
+    printf("%d",rev);
+    return 0;
+}
